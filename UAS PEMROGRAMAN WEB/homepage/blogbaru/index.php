5d6742037450405093fd71f9e43e6f5796bfce0b
kuncara15 <89762832+kuncara15@users.noreply.github.com>
2022-12-22 20:40:00 +0700

    Add files via upload

---
+++ UAS PEMROGRAMAN WEB/homepage/blogbaru/index.php
@@ -0,0 +1,178 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>My Blog</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700|Raleway:300,400,400i,500,500i,700,800,900" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/nivo-slider/css/nivo-slider.css" rel="stylesheet">
+  <link href="lib/owlcarousel/owl.carousel.css" rel="stylesheet">
+  <link href="lib/owlcarousel/owl.transitions.css" rel="stylesheet">
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/venobox/venobox.css" rel="stylesheet">
+
+  <!-- Nivo Slider Theme -->
+  <link href="css/nivo-slider-theme.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <!-- Responsive Stylesheet File -->
+  <link href="css/responsive.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="style10.css">
+
+
+  <!-- =======================================================
+    Theme Name: eBusiness
+    Theme URL: https://bootstrapmade.com/ebusiness-bootstrap-corporate-template/
+    Author: BootstrapMade.com
+    License: https://bootstrapmade.com/license/
+  ======================================================= -->
+</style>
+
+</head>
+
+<body data-spy="scroll" data-target="#navbar-example">
+
+  <div id="preloader"></div>
+
+  <header>
+    <!-- header-area start -->
+    <div id="sticker" class="header-area" style="background-color: #94618E;">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 col-sm-12">
+
+            <!-- Navigation -->
+            <nav class="navbar navbar-default" >
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="navbar-header">
+                <a class="navbar-brand page-scroll sticky-logo" href="index.html">
+                  <h1><span style="color: pink;">G</span>OWES</h1>
+                  <!-- Uncomment below if you prefer to use an image logo -->
+                  <!-- <img src="img/logo.png" alt="" title=""> -->
+								</a>
+              </div>
+              <!-- Collect the nav links, forms, and other content for toggling -->
+              <div class="collapse navbar-collapse main-menu bs-example-navbar-collapse-1" id="navbar-example">
+                <ul class="nav navbar-nav navbar-right">
+                </ul>
+              </div>
+              <!-- navbar-collapse -->
+            </nav>
+            <!-- END: Navigation -->
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- header-area end -->
+  </header>
+  <!-- header end -->
+
+  <!-- Start Bottom Header -->
+  <div class="header-bg page-area">
+    <div class="home-overly"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+          <div class="slider-content text-center">
+            <div class="header-bottom">
+              <div class="layer2 wow zoomIn" data-wow-duration="1s" data-wow-delay=".4s">
+                <h1 class="title2">Gowes Blog</h1>
+              </div>
+              <div class="layer3 wow zoomInUp" data-wow-duration="2s" data-wow-delay="1s">
+                <h2 class="title3">Welcome to Our Blog Page</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- END Header -->
+  <div class="blog-page area-padding" style="background-color: #f4decb;">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+			<?php include"kiri.php"; ?>
+        </div>
+        <!-- End left sidebar -->
+        <!-- Start single blog -->
+		<?php
+			include"beranda.php";
+		?>
+      </div>
+    </div>
+  </div>
+  <!-- End Blog Area -->
+
+  <div class="clearfix"></div>
+
+  <!-- Start Footer bottom Area -->
+  
+          <!-- end single footer -->
+          <footer class="page-footer" style="font-family: 'Poppins', sans-serif; background-color: #222; padding: 50px; color: #ccc;">
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-8 col-md-8 col-sm-12" >
+                  <h4 class="text-uppercase font-weightbold" style="color: #ccc;"><b>GOWES</b></h4>
+                  <p style="color: #ccc">Pusat Peminjaman Sepeda Terlengkap & Terpercaya No. 1 di Solo</p>
+                  <h6 style="color: #ccc"><b>GOWES Solo (Main Store)</b></h6>
+                  <p style="color: #ccc">Jl. Sumpah Pemuda No.57 Kadipiro, Banjarsari, Surakarta. <br> GOWES berkomitmen untuk selalu memberikan pelayanan terbaik kepada Anda.</p>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12">
+                  <h6 class="text-uppercase font-weightbold" style="color: #ccc">Contact</h6>
+                  <p style="color: #ccc">Jl. Sumpah Pemuda No.57 Kadipiro, Banjarsari, Surakarta
+                  <br/>info@GOWES.com
+                  <br/>(0271) 35678
+                  <br/>(0271) 30765</p>
+                <div class="sosmed img-fluid img-fluid">
+                  <a href="https://twitter.com/ystrdyo1?s=08" target="_blank" class="sosial">
+                    <img src="img/twitter.png" alt="logo twitter" width="30px">
+                  </a>
+                  <a href="https://m.facebook.com/radii.radii.9461" target="_blank" class="sosial">
+                    <img src="img/facebook.png" alt="logo facebook" width="30px">
+                  </a>
+                  <a href="https://www.instagram.com/kuncaraput/?hl=id" target="_blank" class="sosial">
+                    <img src="img/instagram.png" alt="logo instagram" width="30px">
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="footer-copyright text-center">© 2022 Copyright: info@GOWES.com</div>
+          </footer>
+
+  <a href="#" class="back-to-top" style:"background-color: #94618E !important;"><i class="fa fa-chevron-up" style:"background-color: #94618E !important;"></i></a>
+
+  <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/venobox/venobox.min.js"></script>
+  <script src="lib/knob/jquery.knob.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/parallax/parallax.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/nivo-slider/js/jquery.nivo.slider.js" type="text/javascript"></script>
+  <script src="lib/appear/jquery.appear.js"></script>
+  <script src="lib/isotope/isotope.pkgd.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD8HeI8o-c1NppZA-92oYlXakhDPYR7XMY"></script>
+
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <script src="js/main.js"></script>
+</body>
+
+</html>
