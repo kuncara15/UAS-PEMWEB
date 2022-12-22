c8fd8e43058d1c39aca7d6a98709b77c95a59051
kuncara15 <89762832+kuncara15@users.noreply.github.com>
2022-12-22 20:20:14 +0700

    Add files via upload

---
+++ blogbaru/isi4.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    
+    <head>
+      
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <style type="text/css">
+            
+            .no-js #loader { display: none;  }
+            .js #loader { display: block; position: absolute; left: 100px; top: 0; }
+            .se-pre-con {
+                position: fixed;
+                left: 0px;
+                top: 0px;
+                width: 100%;
+                height: 100%;
+                z-index: 9999;
+                background: url(views/Preloader_2.gif) center no-repeat #fff;
+            }
+            
+            html{
+                scroll-behavior: smooth;
+            }
+
+            body {
+                -webkit-font-smoothing: antialiased;
+                background-image: url(img/lalala.jpg);
+                background-attachment: fixed;
+                background-size: cover;
+
+            }
+
+            
+            @font-face {
+                font-family: font;
+                src: url(myFont.otf);
+            }
+            .container .card{
+                background-color: #f4decb; 
+                box-shadow: -4px 4px 12px 0 #49274a;
+                margin-bottom: 2.2rem;
+                border-radius: 2px;
+                margin-top: 20px;
+                padding-top: 20px;
+                padding-right: 5%;
+                padding-left: 5%;
+            }
+        
+            .jumbotron {
+                width: 100%;
+                height: 350px;
+                background-color: transparent;
+            }
+            
+            #profile {
+                height: 200px;
+                width: 200px;
+                overflow: hidden;
+            }
+            
+            #profile-img {
+                object-fit: cover;
+                width: 100%;
+                height: auto;
+                transform: scale(1, 1);
+                transition: all .4s ease;
+            }
+            
+            #profile-img:hover {
+                transform: scale(1.17, 1.17);
+                opacity: .5;
+            }
+            
+            .footer {
+                position: relative;
+                bottom: 0;
+                width: 100%;
+                height: 60px;
+                line-height: 60px;
+                color: #fff;
+                font-size: 12px;
+            }
+            p, ul{
+                font-size: 1.8vw !important;
+                font-style: normal !important;
+                font-family: 'Poppins', sans-serif;
+            }
+            .img img{
+                width: 50%;
+            }
+            .container{
+                color: #94618e;
+                text-align: justify;
+            }
+        
+        </style>
+      
+    </head>
+    <body>
+        
+        <div class="se-pre-con" id='top'></div>
+        
+        <div class="jumbotron jumbotron-fluid p-0 mb-0">
+            <div class="container-fluid text-center m-0 p-5" style='padding-top: 100px !important; height: 100%; background-color: rgba(0, 0, 0, 0.6); color: #fff;'>
+                <h1 style='font-family: font; font-size: 75px;'>Article&nbsp;</h1>
+                <p class="lead muted" style='letter-spacing: 9px !important;'>Milani Tanya.Q.</p>
+            </div>
+        </div>
+        
+        <div class="fluid-container m-0 p-0" style='background-color: #fff;'>
+            <div class="container pt-5 pb-5">
+                <div class="rounded-circle" id='profile' style='position: relative; margin: 0 auto; background-color: #000;'>
+                    <img id="profile-img" src='img/milani.jpeg'>
+                </div>
+                    <div class="card col-md-12">
+                        <center><h5><b>KEEP, SURAKARTA, BICYCLE</b></h5>
+                        <h1>Tips Merawat Sepeda Listrik Agar Awet</h1>
+                        <div class="blog-meta">
+                          <span class="comments-type">
+                              <i class="fa fa-commenting" aria-hidden="true"></i> 0 comments 
+                            </span>
+                          <span class="date-type">
+                              <i class="fa fa-calendar"></i> 2022-11-11 / 01:11:01
+                            </span>
+                            <span class="date-type">
+                              <i class="fa fa-user" aria-hidden="true"></i> Milani Tanya.Q.
+                            
+                        </div></center><br>
+                        <div class="img"> 
+                            <center><img src="img/artikel/4.png" alt=""></center>
+                        </div>
+
+
+                
+                        <p class="lead mt-5">
+                            <h4>1. Cek Rantai Sepeda</h4>
+                            <ul type="square">
+                                Ban pada sepeda listrik perlu diperhatikan karena tekanan ban bisa berdampak pada konsumsi baterai dimana semakin lunak baterai sepeda, semakin berat juga gerak sepeda. Jika tekanan ban kurang optimal maka sepeda juga jadi lebih sulit dikendalikan, khususnya ketika dibawa ke medan yang sulit. Ban yang terisi cukup udara juga berperforma lebih baik saat melakukan pengereman. Pastikan saat memompa ban sepeda listrik, kamu juga harus memperhatikan PSI yang terdapat pada ban ya. Untuk kamu yang lagi cari pompa lantai yang sudah dilengkapi dengan gauge atau alat ukur, kami rekomendasikan pompa Polygon Pompa Lantai STX.
+                            </ul>
+
+                            <h4>2. Tekanan Ban Harus Dijaga</h4>
+                            <ul type="square">
+                                Pada sepeda komponen rantai adalah komponen yang harus diperhatikan. Karena rantai menghubungkan mesin motor dengan roda belakang, khususnya pada sepeda listrik yang menggunakan mesin motor mid-drive. Agar awet pesepeda bisa memilih pelumas kering atau basah untuk melumasi rantai sepeda, apa bedanya? Pelumas basah umumnya bertahan lebih lama, bahkan saat bersepeda dalam kondisi buruk sementara pelumas kering tidak memiliki ketahanan seperti pelumas basah, tetapi cenderung meninggalkan hasil lebih bersih.
+                                Pengecekan rantai juga harus rutin dilakukan untuk melihat apakah rantai melar atau tidak, hal ini dilakukan karena rantai sepeda listrik dilewati tegangan dua kali lebih besar dibanding sepeda non-elektrik. Pengecekan ini bisa dilakukan dengan menggunakan checker rantai sepeda. Checker rantai sepeda adalah alat yang digunakan untuk mengetahui kelayakan kondisi rantai.
+                            </ul>
+
+                            <h4>3. Cas Baterai Sepeda Listrik Berkala</h4>
+                            <ul type="square">
+                                jika SobatSS ada keinginan untuk mencuci sepeda listrik, maka tips dari kami adalah, sebisa mungkin jangan langsung menyemprot sepeda listrik dengan air secara langsung ke bagian seperti foot pad dan kontroler, karena bagian foot pad di dalamnya terdapat sebuah aki yang bisa korslet jika tersemprot air. Kamu bisa menggunakan lap basah untuk mengelap bagian  ya. Namun tentu pada bagian ban, kamu tetap bisa membasuhnya dengan air kok! Tetapi dengan intensitas air yang rendah ya!
+                            </ul>
+
+                            <h4>4. Cek Kualitas Rem</h4>
+                            <ul type="square">
+                                Pastikan rem tetap pakem ya, jika saat pengereman pada brake lever dirasa cukup jauh atau tidak pakem maka yang bisa dilakukan adalah putar setelan baut yang ada di roda depan searah jarum jam.
+                            </ul></p>
+
+                            <p>Sepeda listrik memiliki komponen yang khusus yang tidak ada pada sepeda biasa yakni baterai dan motor listrik. Oleh karena itu sepeda listrik membutuhkan perawatan lebih ekstra dan lebih rutin dari sepeda biasa. Selain itu cara merawatnya pun sedikit berbeda dari sepeda nonelektrik. Nah, berikut beberapa tips merawat sepeda listrik yang bisa kamu coba di rumah. Itulah tips merawat sepeda listrik agar makin awet. Bagi kamu yang berminat membeli sepeda listrik, SerbaSepeda menjual beragam sepeda listrik mulai dari sepeda lipat listrik, sepeda gunung listrik, dan sepeda hybrid listrik. Semua tipe sepeda listrik tersedia di toko sepeda SerbaSepeda yang berlokasi di Jabodetabek atau bisa melalui toko sepeda online SerbaSepeda.
+
+
+                        </p>
+                        
+                        <blockquote class="blockquote mt-5 mb-5">
+                            <p class="mb-0">maugowes.com, www.anekasepedalistrik.com</p>
+                            <footer class="blockquote-footer" style="color: #94618e;">Referensi terkait Artikel <cite title="Source Title">Gowes</cite></footer>
+                        </blockquote>
+                    </div>
+            </div>
+            
+            <div class="footer fluid-container text-center" style="background-color: #222222;">
+                COPYRIGHT <strong>&copy; <a href="front.php" style='color: #94618e;'>Web ini untuk kepentingan penugasan</a></strong> cr <strong>berlaku untuk setiap referensi</strong> - 2022
+            </div>
+            
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+        
+        <script type="text/javascript">
+        
+            $(window).on('load', function() {
+                $(".se-pre-con").fadeOut("slow");
+            });
+
+            
+        </script>
+        
+    </body>
+    
+</html>
---
+++ blogbaru/kiri.php
@@ -0,0 +1,78 @@
+ <div class="page-head-blog">
+            <div class="single-blog-page">
+              <!-- search option start -->
+              <form action="#">
+                
+              </form>
+              <!-- search option end -->
+            </div>
+            <div class="single-blog-page">
+              <!-- recent start -->
+              <div class="left-blog">
+                <h4>recent post</h4>
+                <div class="recent-post">
+                  <!-- start single post -->
+                  <div class="recent-single-post">
+                    <div class="post-img">
+                      <a href="isi1.php">
+												   <img src="img/artikel/1.jpg" alt="">
+												</a>
+                    </div>
+                    <div class="pst-content">
+                      <p><a href="isi1.php"> 2022 - Sepeda Listrik Harley Davidson Serial 1 E-bikeY </a></p>
+                    </div>
+                  </div>
+                  <div class="recent-single-post">
+                    <div class="post-img">
+                      <a href="isi2.php">
+												   <img src="img/artikel/2.jpg" alt="">
+												</a>
+                    </div>
+                    <div class="pst-content">
+                      <p><a href="isi2.php"> 2022 - 5 Bahaya Bersepeda Saat Hujan, Bisa Dihindari Sekarang! </a></p>
+                    </div>
+                  </div>
+                  <div class="recent-single-post">
+                    <div class="post-img">
+                      <a href="isi3.php">
+												   <img src="img/artikel/3.jpg" alt="">
+												</a>
+                    </div>
+                    <div class="pst-content">
+                      <p><a href="isi3.php"> 2022 - Perbedaan Sepeda Balap, Hybrid dan Gravel </a></p>
+                    </div>
+                  </div>
+                  <div class="recent-single-post">
+                    <div class="post-img">
+                      <a href="isi4.php">
+												   <img src="img/artikel/4.png" alt="">
+												</a>
+                    </div>
+                    <div class="pst-content">
+                      <p><a href="isi4.php"> 2022 - Tips Merawat Sepeda Listrik Agar Awet </a></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="single-blog-page">
+              <div class="left-blog">
+                <h4>categories</h4>
+                <ul>
+                  <li>
+                    <a href="isi1.php">Technologies</a>
+                  </li>
+                  <li>
+                    <a href="isi2.php">Healthy</a>
+                  </li>
+                  <li>
+                    <a href="isi3.php">Design</a>
+                  </li>
+                  <li>
+                    <a href="isi4.php">Tips & Trik</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            
+          </div>
