2eafea3f38a18487ad74c28cca4d2cdbe5fa3b25
kuncara15 <89762832+kuncara15@users.noreply.github.com>
2022-12-22 20:31:53 +0700

    Add files via upload

---
+++ UAS PEMROGRAMAN WEB/homepage/sepeda.sql
@@ -0,0 +1,314 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 21 Des 2022 pada 12.09
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sepeda`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(4) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `telp` varchar(12) NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `password` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `nama`, `email`, `alamat`, `telp`, `username`, `password`) VALUES
+(1, 'admin', 'admin@gmail.com', 'solo', '08123456789', 'admin', '21232f297a57a5a743894a0e4a801fc3');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_transaksi`
+--
+
+CREATE TABLE `detail_transaksi` (
+  `id_detail` int(11) NOT NULL,
+  `id_transaksi` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jenis`
+--
+
+CREATE TABLE `jenis` (
+  `id_jenis` int(4) NOT NULL,
+  `nama` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `jenis`
+--
+
+INSERT INTO `jenis` (`id_jenis`, `nama`) VALUES
+(1, 'ROADBIKE'),
+(2, 'MOUNTAIN BIKE'),
+(3, 'TOURING BIKE'),
+(4, 'SEPEDA LIPAT'),
+(5, 'SEPEDA LISTRIk'),
+(8, 'SEPEDA ANAK'),
+(9, 'SEPEDA JADUL');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `konfirmasi`
+--
+
+CREATE TABLE `konfirmasi` (
+  `id_konfirmasi` int(11) NOT NULL,
+  `id_transaksi` int(11) NOT NULL,
+  `bukti` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `konfirmasi`
+--
+
+INSERT INTO `konfirmasi` (`id_konfirmasi`, `id_transaksi`, `bukti`) VALUES
+(12, 14, '421122022111219.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `id_pelanggan` int(11) NOT NULL,
+  `no_ktp` char(16) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `no_telp` char(12) NOT NULL,
+  `alamat` varchar(50) DEFAULT NULL,
+  `username` varchar(10) NOT NULL,
+  `password` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `pelanggan`
+--
+
+INSERT INTO `pelanggan` (`id_pelanggan`, `no_ktp`, `nama`, `email`, `no_telp`, `alamat`, `username`, `password`) VALUES
+(4, '19202', 'agy', 'snakn@gmail.com', '219837', 'surakarta', 'agy', 'bded9ffb48f592e442f5c38b72b68eeb');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `sepeda`
+--
+
+CREATE TABLE `sepeda` (
+  `id_sepeda` int(11) NOT NULL,
+  `id_jenis` int(4) NOT NULL,
+  `no_sepeda` varchar(10) NOT NULL,
+  `merk` varchar(20) NOT NULL,
+  `nama_sepeda` varchar(30) NOT NULL,
+  `gambar` varchar(100) NOT NULL,
+  `harga` int(7) NOT NULL,
+  `status` enum('0','1') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `sepeda`
+--
+
+INSERT INTO `sepeda` (`id_sepeda`, `id_jenis`, `no_sepeda`, `merk`, `nama_sepeda`, `gambar`, `harga`, `status`) VALUES
+(11, 2, 'SP1', 'Polygon', 'Premiere 4', '21122022174130.png', 80000, '1'),
+(12, 1, 'SP2', 'Polygon', 'MY20 STRATTOS', '21122022174157.png', 120000, '1'),
+(13, 3, 'SP3', 'Polygon', 'BEND RIV', '21122022174306.png', 90000, '1'),
+(14, 4, 'SP4', 'PACIFIC', 'BLITZ 30', '21122022174328.png', 80000, '1'),
+(15, 5, 'SP5', 'SELIS', 'IOI', '21122022174351.png', 150000, '1'),
+(16, 1, 'SP6', 'polymera', 'VREX', '21122022174422.png', 100000, '1'),
+(18, 8, 'SP7', 'Polygon', 'SIERRA LITE', '21122022180327.jpg', 100000, '0'),
+(19, 9, 'SP8', 'Polymera', 'PIT KEBO', '21122022180353.jpg', 70000, '1');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `id_pelanggan` int(11) NOT NULL,
+  `id_sepeda` int(11) NOT NULL,
+  `tgl_sewa` datetime NOT NULL,
+  `tgl_ambil` datetime DEFAULT NULL,
+  `tgl_kembali` datetime DEFAULT NULL,
+  `lama` tinyint(1) NOT NULL,
+  `total_harga` int(7) NOT NULL,
+  `status` enum('0','1') NOT NULL,
+  `jaminan` varchar(30) NOT NULL,
+  `denda` int(7) DEFAULT 0,
+  `jatuh_tempo` datetime NOT NULL,
+  `konfirmasi` enum('0','1') DEFAULT NULL,
+  `pembatalan` enum('0','1') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `id_pelanggan`, `id_sepeda`, `tgl_sewa`, `tgl_ambil`, `tgl_kembali`, `lama`, `total_harga`, `status`, `jaminan`, `denda`, `jatuh_tempo`, `konfirmasi`, `pembatalan`) VALUES
+(14, 4, 13, '2022-12-21 11:12:05', '2022-12-19 11:12:05', '2022-12-21 11:00:00', 1, 90000, '1', 'FC KTP', 23000, '2022-12-21 14:12:05', '1', '0');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indeks untuk tabel `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD PRIMARY KEY (`id_detail`),
+  ADD KEY `fk_transaksi` (`id_transaksi`);
+
+--
+-- Indeks untuk tabel `jenis`
+--
+ALTER TABLE `jenis`
+  ADD PRIMARY KEY (`id_jenis`);
+
+--
+-- Indeks untuk tabel `konfirmasi`
+--
+ALTER TABLE `konfirmasi`
+  ADD PRIMARY KEY (`id_konfirmasi`),
+  ADD KEY `fk_transaksi` (`id_transaksi`);
+
+--
+-- Indeks untuk tabel `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`id_pelanggan`);
+
+--
+-- Indeks untuk tabel `sepeda`
+--
+ALTER TABLE `sepeda`
+  ADD PRIMARY KEY (`id_sepeda`),
+  ADD KEY `fk_jenis` (`id_jenis`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `fk_pelanggan` (`id_pelanggan`),
+  ADD KEY `fk_sepeda` (`id_sepeda`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `jenis`
+--
+ALTER TABLE `jenis`
+  MODIFY `id_jenis` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT untuk tabel `konfirmasi`
+--
+ALTER TABLE `konfirmasi`
+  MODIFY `id_konfirmasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT untuk tabel `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT untuk tabel `sepeda`
+--
+ALTER TABLE `sepeda`
+  MODIFY `id_sepeda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD CONSTRAINT `detail_transaksi_ibfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `konfirmasi`
+--
+ALTER TABLE `konfirmasi`
+  ADD CONSTRAINT `konfirmasi_ibfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `sepeda`
+--
+ALTER TABLE `sepeda`
+  ADD CONSTRAINT `sepeda_ibfk_1` FOREIGN KEY (`id_jenis`) REFERENCES `jenis` (`id_jenis`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id_pelanggan`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ UAS PEMROGRAMAN WEB/index.php
@@ -0,0 +1,237 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>HOME</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&amp">
+    <link rel="stylesheet" href="style10.css">
+  </head>
+<body>
+  <header class="header">
+      <nav class="navbar navbar-expand-lg fixed-top py-3">
+          <div class="container"><a href="#" class="navbar-brand text-uppercase font-weight-bold"><span style="color: pink;">G</span>OWES</a>
+            <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler navbar-toggler-right"><i class="fa fa-bars"></i></button>
+            <div id="navbarSupportedContent" class="collapse navbar-collapse">
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item active"><a href="#top" class="nav-link text-uppercase font-weight-bold" target="_self">Home <span class="sr-only"></span></a></li>
+                <li class="nav-item"><a href="#about" class="nav-link text-uppercase font-weight-bold" target="_self">About US</a></li>
+                <li class="nav-item"><a href="#galeri" class="nav-link text-uppercase font-weight-bold" target="_self">Gallery</a></li>
+                <li class="nav-item"><a href="#contact" class="nav-link text-uppercase font-weight-bold" target="_self">Syarat-Ketentuan & Cara Peminjaman</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+    
+    <section class="box">  
+      <video src="video opening.mp4" autoplay muted loop></video>
+      <h1>WELCOME TO GOWES !</h1> 
+      <h3>Sejak tahun 2013, Gowes telah hadir di kota Solo dan menjadi situs peminjaman transportasi sepeda ramah lingkungan. Gowes menjadi pilihan transportasi terbaik dalam perjalanan anda.</h3>
+      <a href="homepage/pemesanan/index.php" class="boxBtn" >Get Started</a> 
+    </section>
+
+	<section class="about section-padding" id="about">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 col-md-12 col-12 py-3 bg-custom rounded-circle">
+					<div class="about-img"><img alt="logo gowes" class="img-fluid" src="homepage/images/LOGO.png"></div>
+				</div>
+				<div class="col-lg-8 col-md-12 col-12 ps-lg-5 mt-md-3">
+					<div class="about-text text-custom">
+						<h2> <strong>We Provide the Best Quality<br>
+              Services Ever</strong></h2>
+              <hr>
+						<p class="text-justify">Solo sebagai kota wisata dan pelajar, sayangnya tidak memiliki layanan angkutan umum yang ramah lingkungan. Sudah pasti hal ini berdapak pada meningkatnya penggunaan motor dan mobil di dalam kota. Alhasil, wajah ramah Kota Solo perlahan-lahan berubah menjadi kota macet dan semrawut.Terlebih lagi di musim liburan, kota ini dipadati oleh wisatawan domestic dan mancanegara yang memilih menyewa motor dan mobil untuk berwisata. Padahal ada opsi transportasi murah yang dapat mengatasi masalah minimnya ketersediaan angkutan umum di Solo, yaitu <b>SEPEDA</b>. Solo yang memiliki luasan kota relatif kecil(44,02 km) dan permukaan jalan yang relatif datar sangat mendukung untuk beraktifitas dengan sepeda.Kondisi ini memunculkan ide akan perlunya sebuah unit layanan yang sekaligus bisa mendorong orang memilih kendaraan sepeda sebagai alat transportasi selama berada di Solo. Unit usaha ini merupakan upaya untuk memunculkan opsi sepeda sebagai alat transportasi bagi wisatawan dan siapapun yang berkunjung ke Solo. Kenyamanan perjalanan anda menjadi prioritas kami.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+  <div style="background: #BFA5A4"><div class="container features">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-sm-12">
+        <br>
+        <center><img src="homepage/images/a.jpg" width="250px" height="150px"></img></center>
+        <br><p>Cabang 1 GOWES - terletak di Jl. Sumpah Pemuda No.57 Kadipiro, Banjarsari, Surakarta. Kami meminjamkan berbagai macam jenis sepeda. </p>
+      </div>
+        
+      <div class="col-lg-4 col-md-4 col-sm-12">
+        <br>
+        <center><img src="homepage/images/a1.jpg" width="250px" height="150px"></img></center>
+        <br><p>Cabang 2 GOWES - terletak di Jl. Jatipadang Baru No.40 Pajang, Laweyan, Surakarta. Kami meminjamkan berbagai macam jenis sepeda.  </p>
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-12">
+        <br>
+        <center><img src="homepage/images/a2.jpg" width="250px" height="150px"></img></center>
+        <br><p style="justify-content: center, align-items: justify;">Cabang 3 GOWES - terletak di Jl. Ahmad Yani No.16 Kentingan, Surakartat. Kami meminjamkan berbagai macam jenis sepeda.</p></br>
+      </div>
+    </div> 
+  </div>
+  </div>
+
+  <div style="background: #BFA5A4"><div class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-7 text-center mb-3 mb-lg-0 text-lg-start">
+          <h3 class="text-black m-0">Begin your adventurous journey here.</h3>
+        </div>    
+        <div class="col-lg-5 text-center text-lg-end">
+          <a href="homepage/contact.php" class="btn btn-outline-white"><h4>Contact Us</h4></a>
+          <a href="homepage/blogbaru/index.php" class="btn btn-outline-white"><h4>Our Blog</h4></a>
+        </div>
+      </div>    
+    </div>    
+  </div>
+  </div> 
+
+    <div class="container section-title" id="galeri">
+      <h2 class=" text-center text-lg-start mt-4 mb-5">Gallery</h2>
+      <div class="row text-center text-lg-start">
+        <div class="col-lg-3 col-md-4 col-6">
+          <a class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="homepage/images/galeri1.jpg" alt="">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <a class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="homepage/images/galeri2.jpg" alt="">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <a class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="homepage/images/galeri3.jpg" alt="">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <a class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="homepage/images/galeri4.jpg" alt="">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <a class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="homepage/images/galeri5.jpg" alt="">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <a class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="homepage/images/galeri6.jpg" alt="">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <a class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="homepage/images/galeri7.jpg" alt="">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <a class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="homepage/images/galeri8.jpg" alt="">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <a class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="homepage/images/galeri9.jpg" alt="">
+            </a>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <a class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="homepage/images/galeri10.jpg" alt="">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <a class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="homepage/images/galeri11.jpg" alt="">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <a class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="homepage/images/galeri12.jpg" alt="">
+          </a>
+        </div>
+      </div>
+    </div>
+    
+    <section id="contact" class="contact">
+      <div class="container">
+        <div class="section-title">
+          <h2>Syarat-Ketentuan dan Cara Peminjaman</h2>
+          <p class="px-5">Berikut merupakan Syarat-Ketentuan dan dan Cara Peminjaman <b><br>GOWES</br></b></p>
+        </div>
+        <div class="row bg-custom">
+          <div class="col-lg-12 d-flex align-items-stretch">
+            <div class="info">
+              <div class="syarat">
+                <h4>Syarat & Ketentuan:</h4>
+                <ul type="square">
+                  <li>Dapat mengandari sepeda dengan baik.</li>
+                  <li>Meninggalkan kartu identitas asli (KTP, SIM, Pasport, Kartu Pelajar).</li>
+                  <li>Memberikan deposit sebesar Rp 200.000 (akan dikembalikan saat peminjam mengembalikan sepeda).</li>
+                  <li>Peminjam akan difoto sebagai bukti peminjaman.</li>
+                  <li>Lama waktu peminjaman maksimal 2 jam.</li>
+                  <li>Akan dikenakan denda apabila melewati batas waktu peminjaman.</li>
+                </ul>
+              </div>
+              <div class="syarat">
+                <h4>Cara Peminjaman:</h4>
+                <ul type="square">
+                  <li>Melakukan registrasi apabila belum memiliki akun.</li>
+                  <li>Melakukan login.</li>
+                  <li>Masuk ke menu sepeda.</li>
+                  <li>Masuk ke menu sepeda.</li>
+                  <li>Ambil sepeda di tempat peminjaman Gowes.</li>
+                  <li>Tunjukkan kode kepada petugas Gowes.</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+          <footer class="page-footer ">
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-8 col-md-8 col-sm-12">
+                  <h4 class="text-uppercase font-weight-bold"><b>GOWES</b></h4>
+                  <p>Pusat Peminjaman Sepeda Terlengkap & Terpercaya No. 1 di Solo</p>
+                  <h6><b>GOWES Solo (Main Store)</b></h6>
+                  <p>Jl. Sumpah Pemuda No.57 Kadipiro, Banjarsari, Surakarta. <br> GOWES berkomitmen untuk selalu memberikan pelayanan terbaik kepada Anda.</p>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12">
+                  <h6 class="text-uppercase font-weight-bold">Contact</h6>
+                  <p>Jl. Sumpah Pemuda No.57 Kadipiro, Banjarsari, Surakarta
+                  <br/>info@GOWES.com
+                  <br/>(0271) 35678
+                  <br/>(0271) 30765</p>
+                <div class="sosmed img-fluid img-fluid">
+                  <a href="https://twitter.com/ystrdyo1?s=08" target="_blank" class="sosial">
+                    <img src="homepage/images/twitter.png" alt="logo twitter" width="30px">
+                  </a>
+                  <a href="https://m.facebook.com/radii.radii.9461" target="_blank" class="sosial">
+                    <img src="homepage/images/facebook.png" alt="logo facebook" width="30px">
+                  </a>
+                  <a href="https://www.instagram.com/kuncaraput/?hl=id" target="_blank" class="sosial">
+                    <img src="homepage/images/instagram.png" alt="logo instagram" width="30px">
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="footer-copyright text-center">© 2022 Copyright: info@GOWES.com</div>
+          </footer>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="homepage/code.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</body>
+</html>
+</body>
+</html>
